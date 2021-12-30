4c16134e5f6c666026fa099386aa6d02fe286bed
bUsernameIsUnavailable <61911973+bUsernameIsUnavailable@users.noreply.github.com>
2021-12-30 23:41:06 +0200

    Build precompiled headers

---
+++ include/PCH.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <soil/SOIL.h>
+
+#include <entt/entt.hpp>
